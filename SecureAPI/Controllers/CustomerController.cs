130acb7aea884123bbaf24d222c8822b7a5adee9
Niraj Trivedi <e075586@GH-3TRPDC2.corp.mastercard.org>
2017-12-29 14:45:04 +1100

    Added API project

---
+++ SecureAPI/Controllers/CustomerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using SecureAPI.Models;
+
+namespace SecureAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public IHttpActionResult Post(Customer customer)
+        {
+            return Ok(customer);
+        }
+    }
+}
