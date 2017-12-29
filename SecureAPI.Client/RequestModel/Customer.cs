07b62e196d179247ed0a1924c7c3de8cbeaa280d
Niraj Trivedi <e075586@GH-3TRPDC2.corp.mastercard.org>
2017-12-29 14:34:49 +1100

    Added customer request model class

---
+++ SecureAPI.Client/RequestModel/Customer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SecureAPI.Client.RequestModel
+{
+    class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string City { get; set; }
+    }
+}
