8e5812986f1637691389b23e613437cbb14bc4a2
Niraj Trivedi <e075586@GH-3TRPDC2.corp.mastercard.org>
2018-01-02 11:54:13 +1100

    Updated code

--- SecureAPI/Filters/HMACAuthenticationAttribute.cs
+++ SecureAPI/Filters/HMACAuthenticationAttribute.cs
@@ -2,12 +2,22 @@
 
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
 using System.Web.Http.Filters;
+using System.Web.Http.Results;
 
 namespace SecureAPI.Filters
 {
+// ReSharper disable once InconsistentNaming
     public class HMACAuthenticationAttribute : Attribute, IAuthenticationFilter
     {
         private static Dictionary<string, string> allowedApps = new Dictionary<string, string>();
@@ -40,21 +50,28 @@ namespace SecureAPI.Filters
                     var nonce = authorizationHeaderArray[2];
                     var requestTimeStamp = authorizationHeaderArray[3];
 
-                    var isValid = isValidaRequest(request, APPId, base64Signature, nonce, requestTimeStamp);
+                    var isValid = IsValidRequest(request, APPId, base64Signature, nonce, requestTimeStamp);
                     if (isValid.Result)
                     {
-                        
+                        var currentPrincipal = new GenericPrincipal(new GenericIdentity(APPId), null);
+                        context.Principal = currentPrincipal;
+                    }
+                    else
+                    {
+                        context.ErrorResult = new UnauthorizedResult(new AuthenticationHeaderValue[0], context.Request);
                     }
                 }
+                else
+                {
+                    context.ErrorResult = new UnauthorizedResult(new AuthenticationHeaderValue[0], context.Request);
+                }
             }
-            
+            else
+            {
+                context.ErrorResult = new UnauthorizedResult(new AuthenticationHeaderValue[0], context.Request);
+            }
+            return Task.FromResult(0);
         }
-
-        private async Task<bool> isValidaRequest(System.Net.Http.HttpRequestMessage request, string APPId, string base64Signature, string nonce, string requestTimeStamp)
-        {
-            throw new NotImplementedException();
-        }
-
         private string[] GetAuthorizationHeaderArray(string authenticationHeaderParam)
         {
             var splitValue = authenticationHeaderParam.Split(':');
@@ -70,6 +87,108 @@ namespace SecureAPI.Filters
             throw new NotImplementedException();
         }
 
+        private async Task<bool> IsValidRequest(HttpRequestMessage req, string APPId, string incomingBase64Signature, string nonce, string requestTimeStamp)
+        {
+            string requestContentBase64String = "";
+            string requestUri = HttpUtility.UrlEncode(req.RequestUri.AbsoluteUri.ToLower());
+            string requestHttpMethod = req.Method.Method;
+
+            if (!allowedApps.ContainsKey(APPId))
+            {
+                return false;
+            }
+
+            var sharedKey = allowedApps[APPId];
+
+            if (isReplayRequest(nonce, requestTimeStamp))
+            {
+                return false;
+            }
+
+            byte[] hash = await ComputeHash(req.Content);
+
+            if (hash != null)
+            {
+                requestContentBase64String = Convert.ToBase64String(hash);
+            }
+
+            string data = String.Format("{0}{1}{2}{3}{4}{5}", APPId, requestHttpMethod, requestUri, requestTimeStamp, nonce, requestContentBase64String);
+
+            var secretKeyBytes = Convert.FromBase64String(sharedKey);
+
+            byte[] signature = Encoding.UTF8.GetBytes(data);
+
+            using (HMACSHA256 hmac = new HMACSHA256(secretKeyBytes))
+            {
+                byte[] signatureBytes = hmac.ComputeHash(signature);
+
+                return (incomingBase64Signature.Equals(Convert.ToBase64String(signatureBytes), StringComparison.Ordinal));
+            }
+
+        }
+
+        private bool isReplayRequest(string nonce, string requestTimeStamp)
+        {
+            if (System.Runtime.Caching.MemoryCache.Default.Contains(nonce))
+            {
+                return true;
+            }
+
+            DateTime epochStart = new DateTime(1970, 01, 01, 0, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan currentTs = DateTime.UtcNow - epochStart;
+
+            var serverTotalSeconds = Convert.ToUInt64(currentTs.TotalSeconds);
+            var requestTotalSeconds = Convert.ToUInt64(requestTimeStamp);
+
+            if ((serverTotalSeconds - requestTotalSeconds) > requestMaxAgeInSeconds)
+            {
+                return true;
+            }
+
+            System.Runtime.Caching.MemoryCache.Default.Add(nonce, requestTimeStamp, DateTimeOffset.UtcNow.AddSeconds(requestMaxAgeInSeconds));
+
+            return false;
+        }
+
+        private static async Task<byte[]> ComputeHash(HttpContent httpContent)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] hash = null;
+                var content = await httpContent.ReadAsByteArrayAsync();
+                if (content.Length != 0)
+                {
+                    hash = md5.ComputeHash(content);
+                }
+                return hash;
+            }
+        }
+
         
+    }
+
+
+
+    public class ResultWithChallenge : IHttpActionResult
+    {
+        private readonly string authenticationScheme = "amx";
+        private readonly IHttpActionResult next;
+
+        public ResultWithChallenge(IHttpActionResult next)
+        {
+            this.next = next;
+        }
+
+        public async Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = await next.ExecuteAsync(cancellationToken);
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                response.Headers.WwwAuthenticate.Add(new AuthenticationHeaderValue(authenticationScheme));
+            }
+
+            return response;
+        }
     }
 }
