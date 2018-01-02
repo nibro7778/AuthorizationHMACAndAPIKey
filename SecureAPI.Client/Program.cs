4d0223008e312cd1203aec37e1fcc9631ba350b4
Niraj Trivedi <e075586@GH-3TRPDC2.corp.mastercard.org>
2018-01-02 12:48:52 +1100

    Updated code

--- SecureAPI.Client/Program.cs
+++ SecureAPI.Client/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace SecureAPI.Client
+﻿using SecureAPI.Client.RequestModel;
+namespace SecureAPI.Client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            RunAsync().Wait();
+        }
 
+        static async Task RunAsync()
+        {
+
+            Console.WriteLine("Calling the back-end API");
+
+            string apiBaseAddress = "http://localhost:43326/";
+
+            CustomDelegatingHandler customDelegatingHandler = new CustomDelegatingHandler();
+
+            HttpClient client = HttpClientFactory.Create(customDelegatingHandler);
+
+            var customer = new Customer { CustomerId = 1, CustomerName="Niraj", City = "Sydney" };
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(apiBaseAddress + "api/v1/Customers", customer);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseString = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseString);
+                Console.WriteLine("HTTP Status: {0}, Reason {1}. Press ENTER to exit", response.StatusCode, response.ReasonPhrase);
+            }
+            else
+            {
+                Console.WriteLine("Failed to call the API. HTTP Status: {0}, Reason {1}", response.StatusCode, response.ReasonPhrase);
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- SecureAPI/Controllers/CustomerController.cs
+++ SecureAPI/Controllers/CustomerController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Web.Http;
 using SecureAPI.Models;
+using SecureAPI.Filters;
 
 namespace SecureAPI.Controllers
 {
+    [HMACAuthentication]
     [RoutePrefix("api/v1/Customers")]
     public class CustomerController : ApiController
     {
